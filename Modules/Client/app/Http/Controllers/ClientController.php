2c538bf18d12a27cbfb45b1291ddac7d5f53278d
Michel Naoki <michel.naoki@gmail.com>
2024-08-14 08:29:28 -0300

    add alert

--- Modules/Client/app/Http/Controllers/ClientController.php
+++ Modules/Client/app/Http/Controllers/ClientController.php
@@ -3,9 +3,7 @@
 namespace Modules\Client\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Modules\Client\Http\Requests\ClientStoreRequest;
 use Modules\Client\Http\Requests\ClientUpdateRequest;
 use Modules\Client\Repositories\ClientRepository;
